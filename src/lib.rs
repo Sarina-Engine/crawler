77f187b549b70aea66d953eed80a76376c6964ab
Kave Bahraman <sadegh.b218@gmail.com>
2023-01-16 10:42:18 +0330

    fixes minors bugs

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,8 @@ pub mod services;
 
 use std;
 
-pub mod product_send {
-    tonic::include_proto!("product_send");
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     services::get_categories().await?;
     Ok(())
-}
-
-fn get_product_id(url: &str) -> &str {
-    url.split("/").nth(5).unwrap()
 }
